ea5988fff141056ab92a8674b9f1fcbf86851c84
ngotiendat3455 <datngotien345@gmail.com>
2024-11-23 21:06:42 +0700

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+    @Get()
+    public getUsers(){
+        return "You sent a get request to users endpoint";
+    }
+
+    @Post()
+    public createUsers(){
+        return "You sent a post request to users endpoint";
+    }
+}
